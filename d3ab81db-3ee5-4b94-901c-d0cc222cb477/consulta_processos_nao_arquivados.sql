9d26a4a25378f9818aa319b13dc0bfc80dfd6f9d
UNCISAL\josecicero.rocha <jose.cicero@uncisal.edu.br>
2017-08-22 15:39:55 -0300

    Sql consultar processos não arquivados no SEARF

--- d3ab81db-3ee5-4b94-901c-d0cc222cb477/consulta_processos_nao_arquivados.sql
+++ d3ab81db-3ee5-4b94-901c-d0cc222cb477/consulta_processos_nao_arquivados.sql
@@ -43,3 +43,46 @@ GROUP BY
 #   ,item_atas.precoreg
 #   ,item_processos.qtd
 #   ,setors.sigla
+;
+
+
+
+select
+        max(follow_processos.data) as maiordata
+        ,processos.id   as processo_id
+        ,processos.nr_processo  as processo
+        ,processos.dataprocesso
+        ,atas.arp
+        ,atas.vigencia
+        ,atas.id as ata_id
+        ,objetos.objeto
+        ,sum(item_processos.qtd * item_atas.precoreg)  as valor
+        ,sum(item_empenhos.qtd * item_atas.precoreg) as empenho
+        ,(sum(item_processos.qtd * item_atas.precoreg) - sum(item_empenhos.qtd * item_atas.precoreg)) as saldo
+      from
+        processos
+        LEFT JOIN follow_processos ON processos.id = follow_processos.processo_id
+        INNER JOIN item_processos ON processos.id = item_processos.processo_id
+        LEFT JOIN setors ON follow_processos.setor_id = setors.id
+        INNER JOIN atas ON processos.ata_id = atas.id
+        INNER JOIN item_atas ON item_processos.item_ata_id = item_atas.id
+        INNER JOIN objetos ON atas.objeto_id = objetos.id
+        LEFT JOIN empenhos ON processos.id = empenhos.processo_id
+        LEFT JOIN item_empenhos ON empenhos.id = item_empenhos.empenho_id
+        INNER JOIN
+  (SELECT follow_processos.processo_id, MAX(follow_processos.data) as maiordata FROM follow_processos GROUP BY follow_processos.processo_id) as follow
+    ON follow_processos.processo_id = follow.processo_id
+      and follow_processos.data = follow.maiordata
+      where
+        item_processos.qtd > 0
+        and setors.arquivo is NULL
+#         and processos.id = 230
+      GROUP BY
+        processos.id
+        ,processos.nr_processo
+        ,processos.dataprocesso
+        ,atas.arp
+        ,atas.vigencia
+        ,atas.id
+        ,objetos.objeto
+ORDER BY processos.dataprocesso DESC
