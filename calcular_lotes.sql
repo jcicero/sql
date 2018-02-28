605ee56038c3b824d411f0949d93732fc89d1150
José Cícero Rocha Cavalcante <jcicero@gmail.com>
2018-02-28 11:57:59 -0300

    Update calcular_lotes.sql

--- calcular_lotes.sql
+++ calcular_lotes.sql
@@ -1,16 +1,23 @@
 select
-  l.produto_id
-  ,p.produto
+  p.codigo as codigo
+  ,p.produto as descricao
   ,l.lote
-  ,sum(l.qtd) saldo
+  ,date_format(l.validade,'%d/%m/%Y') as validade
+  ,sum(l.qtd) as saldo
+  ,'' as contagem
+  ,'' as recontagem
 from
   produtos p
   left JOIN lotes l ON p.id = l.produto_id
-  WHERE l.produto_id = 218
+  LEFT JOIN produto_estoques pe ON p.id = pe.produto_id
+  WHERE
+    pe.estoque_id = 4
+  and p.deleted_at IS NULL
+  #  and l.produto_id = 340
 group by
   l.lote
   ,l.produto_id
   ,p.produto
-ORDER BY p.produto
+ORDER BY p.produto;
 
 SELECT * from lotes WHERE produto_id = 218
