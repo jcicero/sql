ca2a71ce02f8ed220cf30da46208b4fe284e7a1a
jcicero@gmail.com <csw2ylx9>
2018-06-18 09:59:17 -0300

    Atualizar arquivos de consulta

--- lista_medicamentos_negados_unidade_periodo.sql
+++ lista_medicamentos_negados_unidade_periodo.sql
@@ -1,7 +1,7 @@
 select
-   produtos.codigo
-  ,concat(produtos.produto , ' - ', produtos.unidade) as produto
-  ,s.setor
+   produtos.codigo as CODIGO
+  ,concat(produtos.produto , ' - ', produtos.unidade) as PRODUTO
+ # ,pedidos.requisicao
 from
   pedidos
   left join estoques on pedidos.estoque_id = estoques.id
@@ -9,7 +9,18 @@ from
   left join setors as s on pedidos.setor_id = s.id
   left join produtos on saida.produto_id = produtos.id
 where saida.qtd = 0
-    and pedidos.setor_id = 14 #informar o setor ou comentar para listar todos
-    and estoques.id = 4 #informar o estoque
-    and pedidos.datapedido between '2018-04-01' and '2018-04-30' #informar período a ser pesquisado
+    and pedidos.setor_id = 7 #informar o setor ou comentar para listar todos
+    and estoques.id = 4 #informar o estoque (4 medicamentos)
+    and pedidos.datapedido between '2018-04-22' and '2018-04-28' #informar período a ser pesquisado
+group by
+    produtos.codigo
+    ,produtos.produto
+    ,produtos.unidade
+   # ,pedidos.requisicao
 order by produtos.produto;
+
+# 1 - HEPR
+# 5 - MESM
+# 7 - HEHA
+# 14 - CAPSCV
+# 15 - CERIII
